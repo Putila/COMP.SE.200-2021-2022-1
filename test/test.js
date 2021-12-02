6ce6e5887892c24884acf952d859b34bf250d528
Sanni <onkalosanni@gmail.com>
2021-12-02 20:07:13 +0200

    add tests for filter.js and words.js

--- test/test.js
+++ test/test.js
@@ -11,8 +11,6 @@ import toString from '../src/toString.js';
 
 import { expect } from 'chai';
 
-// add, capitalize, words, filter, divide, isEmpty, ceil, drop, toInteger, toString
-
 describe('add.js', () => {
     it('should return a number', async () => {
         const augend = 4;
@@ -52,25 +50,68 @@ describe('capitalize.js', () => {
     });
 });
 
+describe('words.js', () => {
+    it('should return an array', async () => {
+        const string = 'fred, barney, & pebbles';
+        const wordsArray = words(string);
+        expect(wordsArray).to.be.a('array');
+    });
+    it('should return an array with a length of 3', async () => {
+        const string = 'fred, barney, & pebbles';
+        const wordsArray = words(string);
+        expect(wordsArray).to.have.lengthOf(3);
+    });
+    it('should return an array with a length of 4', async () => {
+        const string = 'fred, barney, & pebbles';
+        const regExp = /[^, ]+/g;
+        const wordsArray = words(string, regExp);
+        expect(wordsArray).to.have.lengthOf(4);
+    });
+});
+
 describe('filter.js', () => {
-    it('', async () => {
+    it('should return an array', async () => {
+        const array = [
+            { 'user': 'barney', 'active': true },
+            { 'user': 'fred', 'active': false }
+        ];
+        const filtered = filter(array, ({ active }) => active);
+        expect(filtered).to.be.a('array');
+    });
+    it('should return an array with a length of 1', async () => {
+        const array = [
+            { 'user': 'barney', 'active': true },
+            { 'user': 'fred', 'active': false }
+        ];
+        const filtered = filter(array, ({ active }) => active);
+        expect(filtered).to.have.lengthOf(1);
+    });
+    it('should return an array with a length of 2', async () => {
+        const array = [
+            { 'user': 'barney', 'active': true },
+            { 'user': 'fred', 'active': false },
+            { 'user': 'alice', 'active': true },
+            { 'user': 'bob', 'active': false },
+        ];
+        const filtered = filter(array, ({ active }) => active);
+        expect(filtered).to.have.lengthOf(2);
     });
 });
 
 describe('divide.js', () => {
-    it('Should return 1 with parameters 10 and 10', async () => {
+    it('should return 1 with parameters 10 and 10', async () => {
         const dividend = 10;
         const divisor = 10;
         const quotient = divide(dividend, divisor);
         expect(quotient).to.equal(1);
     });
-    it('Should return 2 with parameters 10 and 5', async () => {
+    it('should return 2 with parameters 10 and 5', async () => {
         const dividend = 10;
         const divisor = 5;
         const quotient = divide(dividend, divisor);
         expect(quotient).to.equal(2);
     });
-    it('Should return 1 with parameters 20 and 5', async () => {
+    it('should return 1 with parameters 20 and 5', async () => {
         const dividend = 20;
         const divisor = 5;
         const quotient = divide(dividend, divisor);
@@ -79,22 +120,22 @@ describe('divide.js', () => {
 });
 
 describe('isEmpty.js', () => {
-    it('Should return true with parameter null', async () => {
+    it('should return true with parameter null', async () => {
         const value = null;
         const empty = isEmpty(value);
         expect(empty).to.be.true;
     });
-    it('Should return false with a value as parameter', async () => {
+    it('should return false with a value as parameter', async () => {
         const value = 'deez nuts';
         const empty = isEmpty(value);
         expect(empty).to.be.false;
     });
-    it('Should return false with an array that has elements', async () => {
+    it('should return false with an array that has elements', async () => {
         const value = ['deez', 'nuts', 'ha', 'gottem'];
         const empty = isEmpty(value);
         expect(empty).to.be.false;
     });
-    it('Should return true with an empty array', async () => {
+    it('should return true with an empty array', async () => {
         const value = [];
         const empty = isEmpty(value);
         expect(empty).to.be.true;
@@ -126,13 +167,13 @@ describe('ceil.js', () => {
 });
 
 describe('drop.js', () => {
-    it('Should return [2, 3] with parameter [1, 2, 3] and 1', async () => {
+    it('should return [2, 3] with parameter [1, 2, 3] and 1', async () => {
         const arr = [1, 2, 3];
         const amountToDrop = 1;
         const dropped = drop(arr, amountToDrop);
         expect(dropped).to.include(2, 3);
     });
-    it('Should return [3] with parameter [1, 2, 3] and 2', async () => {
+    it('should return [3] with parameter [1, 2, 3] and 2', async () => {
         const arr = [1, 2, 3];
         const amountToDrop = 2;
         const dropped = drop(arr, amountToDrop);
@@ -140,44 +181,44 @@ describe('drop.js', () => {
     });
 });
 describe('toInteger.js', () => {
-    it('Should return number', async () => {
+    it('should return number', async () => {
         const floatNumber = '';
         const integerResult = toInteger(floatNumber);
         expect(integerResult).to.be.a('number');
     });
-    it('Should return 3 with float 3.2', async () => {
+    it('should return 3 with float 3.2', async () => {
         const floatNumber = 3.2;
         const integerResult = toInteger(floatNumber);
         expect(integerResult).to.equal(3);
     });
-    it('Should return 10 with float 10.123951', async () => {
+    it('should return 10 with float 10.123951', async () => {
         const floatNumber = 10.123951;
         const integerResult = toInteger(floatNumber);
         expect(integerResult).to.equal(10);
     });
-    it('Should return 0 with a string as parameter', async () => {
+    it('should return 0 with a string as parameter', async () => {
         const floatNumber = "my name jeff";
         const integerResult = toInteger(floatNumber);
         expect(integerResult).to.equal(10);
     });
 });
 describe('toString.js', () => {
-    it('Should return a string', async () => {
+    it('should return a string', async () => {
         const input = 1;
         const resultString = toString(input);
         expect(resultString).to.be.a('string');
     });
-    it('Should return "123" with input 123', async () => {
+    it('should return "123" with input 123', async () => {
         const input = 123;
         const resultString = toString(input);
         expect(resultString).to.equal('123');
     });
-    it('Should return "1,2,3" with input [1, 2, 3]', async () => {
+    it('should return "1,2,3" with input [1, 2, 3]', async () => {
         const input = [1, 2, 3];
         const resultString = toString(input);
         expect(resultString).to.equal('1,2,3');
     });
-    it('Should return empty string with input null', async () => {
+    it('should return empty string with input null', async () => {
         const input = null;
         const resultString = toString(input);
         expect(resultString).to.equal('');
